8a2fd33ba8dffc80692019f15d2001ba511e4b45
SteGa3 <s.gagni@live.it>
2021-01-21 15:56:58 +0100

    Setting Vendita - Prodotti

---
+++ Entities/Vendita.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Entities
+{
+    public class Vendita
+    {
+        public int idVendita { get; private set; }
+        public int idCliente { get; private set; }
+        public List<Prod_Inf> prod_Inf { get; private set; }
+        public DateTime dateTime { get; private set; }
+        public int totale { get; private set; }
+
+        public Vendita(int _idCliente, List<Prod_Inf> _prodInfList)
+        {
+            idCliente = _idCliente;
+            prod_Inf = _prodInfList;
+            //idvendita =
+
+        }
+    }
+}
