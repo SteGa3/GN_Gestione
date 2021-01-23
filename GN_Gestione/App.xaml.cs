1714a0e7a7cb7ee596b2a3f0b9de5580266a1674
SteGa3 <s.gagni@live.it>
2021-01-23 01:49:56 +0100

    check esistenza cartella settings

--- GN_Gestione/App.xaml.cs
+++ GN_Gestione/App.xaml.cs
@@ -1,13 +1,31 @@
 ﻿using System;
+using System.IO;
 using Entities;
+using DatalayerCSV;
+using FileManager;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PCLStorage;
 
 namespace GN_Gestione
 {
     public partial class App : Application
     {
+
         private static AppSettings appSettings = new AppSettings();
+        private static IFolder rootFolder = PCLStorage.FileSystem.Current.LocalStorage;
+        private static FileManager.DeviceIO fileManager = new FileManager.DeviceIO();
+        private static readonly string settingsFolder = "settings";
+        private static readonly string settingsName = "mainsettings.csv";
+        private static readonly string devicePath = rootFolder.Path.ToString();
+        private string settingsPath = devicePath + "/" + settingsFolder + "/" + settingsName;
+        
+       
+        
+        
+        
+
+
         //file path
         //Check if file exists
         //Write settings on object
@@ -16,12 +34,27 @@ namespace GN_Gestione
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage (new MainPage());
+            var c = fileManager.FileExists(settingsPath);
+            fileManager.DeleteFolder("/data");
+            if (!c)
+            {
+                fileManager.CreateFolder(settingsFolder);
+                fileManager.CreateFile(settingsFolder + "/" + settingsName);
+                //new NavigationPage(new Impostazioni());
+            }
+
+            MainPage = new NavigationPage(new MainPage());
+            
+            
+            
+
+                                           
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
+            
         }
 
         protected override void OnSleep()
@@ -32,6 +65,12 @@ namespace GN_Gestione
         protected override void OnResume()
         {
             // Handle when your app resumes
+        }
+
+        private bool FileExists(string _path)
+        {
+            DeviceIO device = new DeviceIO();
+            return device.FileExists(_path);
         }
     }
 }
