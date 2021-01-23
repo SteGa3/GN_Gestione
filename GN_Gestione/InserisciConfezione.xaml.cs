92d9504b8457ae3b6d336075bbb615fefe9ad93f
SteGa3 <s.gagni@live.it>
2021-01-23 01:44:54 +0100

    Pagina inserimento confezioni

---
+++ GN_Gestione/InserisciConfezione.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GN_Gestione
+{
+    public partial class InserisciConfezione : ContentPage
+    {
+        public InserisciConfezione()
+        {
+            InitializeComponent();
+        }
+    }
+}
